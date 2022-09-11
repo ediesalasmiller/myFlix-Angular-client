177ade1f4b92759c1a74d1442db88ec324877486
edie <ediesalasmiller@gmail.com>
2022-09-11 14:58:07 -0700

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
+//DEFAULTS BELOW WITH ng new myFlix-Angular-client TO BUILD APP
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
+//ADDITION: simplified API for angular apps , makes possible for client app to communicate w API or server-side
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,6 +15,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
